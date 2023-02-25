fb8fea62665569a4a7a564abad820aca1dfe36c2
Terrence Chung <terrencechung35@gmail.com>
2023-02-24 18:04:26 -0800

    lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    print('Hello')
+    def __init__(self, word):
+        self.word_letters = sorted(letter for letter in word)
+        print(self.word_letters)
+    def match(self, word_list):
+        match_word_list = []
+        for word in word_list:
+            if sorted(letter for letter in word) == self.word_letters:
+                match_word_list.append(word)
+        return match_word_list
